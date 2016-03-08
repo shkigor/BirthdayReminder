f22c1a28cc632d898c37093aa2f0a5936a65e82f
Igor Shkliar <igorsh@default-value.com>
2016-03-08 20:40:17 +0200

    Run command s2-quickstart ck.solo User Role

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -115,3 +115,20 @@ log4j.main = {
            'org.hibernate',
            'net.sf.ehcache.hibernate'
 }
+
+
+// Added by the Spring Security Core plugin:
+grails.plugin.springsecurity.userLookup.userDomainClassName = 'ck.solo.User'
+grails.plugin.springsecurity.userLookup.authorityJoinClassName = 'ck.solo.UserRole'
+grails.plugin.springsecurity.authority.className = 'ck.solo.Role'
+grails.plugin.springsecurity.controllerAnnotations.staticRules = [
+	'/':                ['permitAll'],
+	'/index':           ['permitAll'],
+	'/index.gsp':       ['permitAll'],
+	'/assets/**':       ['permitAll'],
+	'/**/js/**':        ['permitAll'],
+	'/**/css/**':       ['permitAll'],
+	'/**/images/**':    ['permitAll'],
+	'/**/favicon.ico':  ['permitAll']
+]
+
