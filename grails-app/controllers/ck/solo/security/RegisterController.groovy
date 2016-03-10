fc45efcafa2680eae9bc3ce46c81b53acde7dcbd
Igor Shkliar <igorsh@default-value.com>
2016-03-10 21:52:19 +0200

    Set @Secured(['permitAll']) to RegisterController

--- grails-app/controllers/ck/solo/security/RegisterController.groovy
+++ grails-app/controllers/ck/solo/security/RegisterController.groovy
@@ -2,6 +2,6 @@ package ck.solo.security
 
 import grails.plugin.springsecurity.annotation.Secured
 
-@Secured(['ROLE_ADMIN'])
+@Secured(['permitAll'])
 class RegisterController extends grails.plugin.springsecurity.ui.RegisterController {
 }
