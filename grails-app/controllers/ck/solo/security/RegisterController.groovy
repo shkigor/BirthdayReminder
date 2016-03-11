297bc6a181fd35912aacd282c98df9ce89ef52ab
Igor Shkliar <igorsh@default-value.com>
2016-03-11 23:56:59 +0200

    Remove field username from register form and set username=email

--- grails-app/controllers/ck/solo/security/RegisterController.groovy
+++ grails-app/controllers/ck/solo/security/RegisterController.groovy
@@ -1,7 +1,14 @@
 package ck.solo.security
 
 import grails.plugin.springsecurity.annotation.Secured
+import grails.plugin.springsecurity.ui.RegisterCommand
 
 @Secured(['permitAll'])
 class RegisterController extends grails.plugin.springsecurity.ui.RegisterController {
+
+    @Override
+    def register(RegisterCommand registerCommand) {
+        registerCommand.username = registerCommand.email
+        super.register(registerCommand)
+    }
 }
